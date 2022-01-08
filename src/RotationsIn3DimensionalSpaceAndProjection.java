c27d45f7de3b7b1f3e2a4f6065ac15bcb1d47466
Joey Smeets <joeysmeetsss@gmail.com>
2022-01-09 00:02:37 +0100

    Started all the potential classes for the cases.

---
+++ src/RotationsIn3DimensionalSpaceAndProjection.java
@@ -0,0 +1,9 @@
+
+public class RotationsIn3DimensionalSpaceAndProjection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/SystemOfLinearEquations.java
@@ -0,0 +1,9 @@
+
+public class SystemOfLinearEquations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
