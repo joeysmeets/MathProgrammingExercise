5bb65163d478a974cad9d5f53a301b63c55a972b
Joey Smeets <joeysmeetsss@gmail.com>
2022-02-05 22:54:30 +0100

    Started working on task 3. Algorith to iterate over the permutations lexicographically seems to work for the most part, but skips permutations.

--- src/Permutations.java
+++ src/Permutations.java
@@ -7,29 +7,53 @@ import java.util.Arrays;
  * @date   05.02.2021
  */
 public class Permutations {
-	ArrayList<int[]> permutations = new ArrayList<>();
+	public static ArrayList<int[]> permutations = new ArrayList<>();
 	
 	public static void main(String[] args) {	 
-		int[] perm1   = {1,3,2,6,5,4};
-		int[] perm2   = {5,1,3,3,4,3,2,1,4,4,5,6,3,2,2,2,2};
-		int[] perm3   = {57,23,46322,63,27,34,6,7,3532,6563,8,52345};
+//		int[] perm1   = {1,3,2,6,5,4};
+//		int[] perm2   = {5,1,3,3,4,3,2,1,4,4,5,6,3,2,2,2,2};
+//		int[] perm3   = {57,23,46322,63,27,34,6,7,3532,6563,8,52345};
+//		
+//		System.out.println(Arrays.toString(getLehmerCode(perm1)));
+//		System.out.println(Arrays.toString(getLehmerCode(perm2)));
+//		System.out.println(Arrays.toString(getLehmerCode(perm3)));
+//		
+//		System.out.println();
+//		
+//		int[] perm4   = {6,1,4,3,2,3,5,5}; 
+//		int[] perm5   = {5000,2000,1000,4000,3000};
+//		int[] perm6   = {1324421,5235,5432,67452374,26,3,732,7,2437,45238463,2,7,45,84,8,4,23,8,654326,75,43,865,2};
+//		System.out.println(Arrays.toString(perm6));
+//		Arrays.sort(perm6);
+//		
+//		System.out.println(Arrays.toString(getLehmerCode(perm4)));
+//		System.out.println(Arrays.toString(getLehmerCode(perm5)));
+//		System.out.println(Arrays.toString(getLehmerCode(perm6)));
+//		System.out.println(Arrays.toString(perm6));
+//		invertArrayFrom(perm6, 10);
+//		System.out.println(Arrays.toString(perm6));
 		
-		System.out.println(Arrays.toString(getLehmerCode(perm1)));
-		System.out.println(Arrays.toString(getLehmerCode(perm2)));
-		System.out.println(Arrays.toString(getLehmerCode(perm3)));
-		
-		System.out.println();
-		
-		int[] perm4   = {6,1,4,3,2,3,5,5}; 
-		int[] perm5   = {5000,2000,1000,4000,3000};
-		int[] perm6   = {1324421,5235,5432,67452374,26,3,732,7,2437,45238463,2,7,45,84,8,4,23,8,654326,75,43,865,2};
-		System.out.println(Arrays.toString(perm6));
-		Arrays.sort(perm6);
-		
-		System.out.println(Arrays.toString(getLehmerCode(perm4)));
-		System.out.println(Arrays.toString(getLehmerCode(perm5)));
-		System.out.println(Arrays.toString(getLehmerCode(perm6)));
-		System.out.println(Arrays.toString(perm6));
+//		int[] perm7   = {1,2,3,4};
+//		System.out.println(Arrays.toString(perm7));
+//		for (int i = 0; i < 24; i++) {
+//			perm7 = permute(perm7);
+//			System.out.println(Arrays.toString(perm7));
+//		}
+//		
+//		System.out.println();
+//		
+//		int[] perm7   = {1, 2, 4, 3};
+//		System.out.println(Arrays.toString(permute(perm7)));
+//		
+//		for (int i = 0; i < 24; i++) {
+//			perm7 = permute(perm7);
+//			System.out.println(Arrays.toString(perm7));
+//		}
+//		
+//		fillWithAllPermutations(perm7); 
+//		for (int i = 0; i < permutations.size(); i++) {
+//			System.out.println(Arrays.toString(permutations.get(i)));
+//		}
 	} 
 	
 	/**
@@ -39,8 +63,8 @@ public class Permutations {
 	 * li |{j{1,2,...n}|(i, j)Inversion}|.
 	 * 
 	 * Method to get the Lehmer Code from a permutation
-	 * @param  int[] permutation   
-	 * @return int[] Lehmer code associated with the given permutation
+	 * @param  permutation int[] the input permutation 
+	 * @return LehmerCode  int[] the lehmer code associated with the given permutation
 	 */
 	public static int[] getLehmerCode( int[] permutation) {
 		int[] lehmerCode = new int[permutation.length]; // instantiating the output array
@@ -53,8 +77,7 @@ public class Permutations {
 			for (int j = i + 1; j < permutation.length + 1; j++) { // iterate over the right neighbors
 				if (j == permutation.length) { // if j is outside of the array, make sure there is no error or incremented count
 					rightValue = Integer.MAX_VALUE;
-				}
-				else { // otherwise set rightValue to the value of the neighbor of the iteration index
+				} else { // otherwise set rightValue to the value of the neighbor of the iteration index
 					rightValue = permutation[j];
 				}
 				if (leftValue > rightValue) { // if the left value is bigger than the right value, the counter it incremented. 
@@ -76,7 +99,7 @@ public class Permutations {
 	 * @param  int[] permutation you want to permute
 	 * @return int[] permutation belonging to the Lehmer code
 	 */
-	public static int[] getPerm(int[] lehmerCode, int[] ogPermutation) {
+	public static void getPerm(int[] lehmerCode, int[] ogPermutation) {
 		int[] permutation = new int[lehmerCode.length]; // instantiating the output array
 
 		ArrayList<Integer> ogPermutationCopy = new ArrayList<Integer>();		
@@ -97,25 +120,83 @@ public class Permutations {
 	 * 3. Schreiben Sie nun eine Methode, welche die Permutationen von {1, 2, ... n} lexikographisch auflistet.
 	 * Tipp: Inkrementieren Sie eine Laufvariable in der oben beschriebenen Darstellung und verwenden Sie diese Zahl als Lehmer-Code für die auszugebende Permutation.
 	 * Rufen Sie in Ihrer Methode eine Methode showPerm auf, welche die Permutation p , ihren Lehmer-Code, ihre Zyklendarstellung und ihr Signum ausgibt.
+	 * Inspired by https://www.quora.com/How-would-you-explain-an-algorithm-that-generates-permutations-using-lexicographic-ordering 
 	 * 
-	 * 
-	 * @param int[] elements
-	 * @return
+	 * @param  permutation int[] the permuation input
+	 * @return 
+	 * @return 
 	 */
-	public ArrayList<int[]> permute(int[] elements) {
-		for (int i = 0; i < elements.length; i++ ) {
-			//swap();
+	public static int[] permute(int[] permutation) {
+		// STEP 1 - highest P[x] < P[x+1]
+		int largestI = -1;
+		for (int i = 0; i < permutation.length - 1; i++) {
+			if ( permutation[i] < permutation[i + 1]) {
+				largestI = i;
+			}
 		}
-		return permutations;
+		// STEP 2 - P[x]<P[y] (largest y possible)
+		int largestJ = -1;
+		for (int j = largestI + 1; j < permutation.length; j++) {
+			if ( permutation[largestI] < permutation[j]) {
+				largestJ = j;
+			}
+		}
+		if (largestI == -1 || largestJ == -1) {
+			return permutation; // no permutation possible. This is permutation in the last lexicographic 
+		}
+		// STEP 3 - Swap P[x] and P[y]
+		permutation = swap(permutation, largestI, largestJ);
+		// STEP 4 = reserve from largestI + 1 to the end
+		permutation = invertArrayFrom(permutation, largestI);
+		// adding the permutation to the ArrayList
+		permutations.add(permutation);
+//		return permutation;
 	}
-}
 	
-//	
-//	public void swap(int[] array, int i, int j) {
-//		int temp = array[i];
-//		array[i] = array[j];
-//		array[j] = temp;
-//	}
+	/**
+	 *
+	 * @param permutation
+	 */
+	public static void fillWithAllPermutations(int[] permutation) {
+		int[] currentPermutation = permutation;
+		int totPermutations = 0;
+		for (int i = 1; i <= permutation.length; i++ ) { // calculating the amount of permutations
+			totPermutations = totPermutations * i;
+		}
+		for (int i = 0; i < totPermutations; i++ ) {
+			permute(currentPermutation);
+		}
+	}
+	
+	/**
+	 * Swaps the array elements of 2 indices
+	 * @param array int[] the array in which elements are swapped
+	 * @param int i element 1
+	 * @param int j element 2
+	 */
+	public static int[] swap(int[] array, int i, int j) {
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+		return array;
+	}
+	
+	/**
+	 * Inverts the array from the starting index
+	 * inspired by https://www.baeldung.com/java-invert-array
+	 * @param array	     int[] array to be reserves
+	 * @param startIndex int   from which index the array has to be reserved
+	 */
+	public static int[] invertArrayFrom(int[] array, int startIndex) {
+	    for (int i = startIndex + 1; i < (array.length + startIndex) / 2; i++) {
+	        Object temp = array[i];
+	        array[i] = array[array.length - 1 - i + startIndex];
+	        array[array.length - 1 - i + startIndex] = (int) temp;
+	    }
+		return array;
+	}
+}	
+	
 
 // OLD VERSION -- STOPS ONCE SOMETHING ON THE RIGHT OF THE CHECKED INTERATION IS HIGHER.
 ///**
